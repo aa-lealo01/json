d8811103c0b9617d148b4447794c53230a3918d4
aa-lealo01 <leonardoi@live.com>
2017-07-10 11:35:20 -0300

    Add files via upload

---
+++ DomainJoin.ps1
@@ -0,0 +1,37 @@
+configuration DomainJoin 
+{ 
+   param 
+    ( 
+        [Parameter(Mandatory)]
+        [String]$domainName,
+
+        [Parameter(Mandatory)]
+        [PSCredential]$adminCreds
+    ) 
+    
+    Import-DscResource -ModuleName xActiveDirectory, xComputerManagement
+
+    $domainCreds = New-Object System.Management.Automation.PSCredential ("$domainName\$($adminCreds.UserName)", $adminCreds.Password)
+   
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature ADPowershell
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        } 
+
+        xComputer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $domainName
+            Credential = $domainCreds
+            DependsOn = "[WindowsFeature]ADPowershell" 
+        }	 
+	}
+}
